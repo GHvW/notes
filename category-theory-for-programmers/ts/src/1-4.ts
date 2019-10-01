0727246f4cd04a44abdbe617c21b480d240af967
Garrett van Wageningen <ghvanw283@gmail.com>
2019-10-01 17:20:35 -0500

    add composition identity test

--- category-theory-for-programmers/ts/src/1-4.ts
+++ category-theory-for-programmers/ts/src/1-4.ts
@@ -9,3 +9,22 @@ function composition<A, B, C>(f1: (x: A) => B, f2: (x: B) => C): (x: A) => C {
         return f2(f1(data));
     }
 }
+
+// 3. use composition to test identity
+function test(): void {
+    const myAge = (age: number) => `I'm ${age} years old.`;
+    const charCount = (s: string) => s.length;
+
+    const idOnA = composition<string, string, number>(identity, charCount);
+    const idOnB = composition(myAge, identity);
+
+    if (idOnA("Hello World!") === charCount("Hello World!")) {
+        console.log("Id on A is good!");
+    }
+
+    if (idOnB(34) === myAge(34)) {
+        console.log("Id on B is good!");
+    }
+}
+
+test();
